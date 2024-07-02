46ca58ecde8ea33a4aab7e8b1263b72e4725559a
codeGOAT99 <taehyun9816@gmail.com>
2024-07-02 16:26:53 +0900

    JS-Header

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//Header scroll dark styling
+const header = document.querySelector(".header");
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > headerHeight) {
+    header.classList.add("header--dark");
+  } else {
+    header.classList.remove("header--dark");
+  }
+});
