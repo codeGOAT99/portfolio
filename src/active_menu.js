57e1c3002aaa48bea3c6c021a436f43b484a6189
codeGOAT99 <taehyun9816@gmail.com>
2024-07-07 18:06:40 +0900

    final

--- src/active_menu.js
+++ src/active_menu.js
@@ -32,7 +32,6 @@ function observerCallback(entries) {
       entry.isIntersecting &&
       entry.intersectionRatio >= 0.95;
   });
-  console.log("무조건 라스트 섹션", selectLastOne);
 
   const navIndex = selectLastOne
     ? sectionsIds.length - 1
